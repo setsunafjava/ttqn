471b9d1096314247c319513d49f3bc8251fae612
quang168 <quang168@0592a2d7-30bf-7e65-cc1e-7c3e7af30aae>
2012-04-13 10:26:46 +0000

    Code get list

--- trunk/CQ.SharePoint.QN/CQ.SharePoint.QN/Webparts/HotNewsContent/HotNewsContentUS.ascx.cs
+++ trunk/CQ.SharePoint.QN/CQ.SharePoint.QN/Webparts/HotNewsContent/HotNewsContentUS.ascx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Data;
 using System.Web.UI;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
 using CQ.SharePoint.QN.Common;
+
 
 namespace CQ.SharePoint.QN.Webparts
 {
@@ -18,7 +20,58 @@ namespace CQ.SharePoint.QN.Webparts
         /// <param name="e">EventArgs e</param>
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            try
+            {
+                //Bind data to latest news
+                string latestNewsQuery = string.Format("<OrderBy><FieldRef Name='Created' Ascending='False' /></OrderBy>");
+                rptLatestNews.DataSource = GetNewsRecords(latestNewsQuery, 5);
+                rptLatestNews.DataBind();
+
+                //Bind data to top view
+                string topNewsQuery = string.Format("<OrderBy><FieldRef Name='{0}' Ascending='False' /></OrderBy>", FieldsName.NewsRecord.English.ViewsCount);
+                rptTopViews.DataSource = GetNewsRecords(topNewsQuery, 5);
+                rptTopViews.DataBind();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Get news record form NewsRecord table
+        /// </summary>
+        /// <param name="query">SPquery for query items</param>
+        /// <param name="newsNumber">number of news want to get</param>
+        /// <returns>News record Datatable</returns>
+        public DataTable GetNewsRecords(string query, uint newsNumber)
+        {
+            DataTable table = new DataTable();
+            SPSecurity.RunWithElevatedPrivileges(() =>
+            {
+                using (var site = new SPSite(SPContext.Current.Web.Site.ID))
+                {
+                    using (var web = site.OpenWeb(SPContext.Current.Web.ID))
+                    {
+                        try
+                        {
+                            SPQuery spQuery = new SPQuery
+                                                {
+                                                    Query = query,
+                                                    RowLimit = newsNumber
+                                                };
+                            SPList list = web.Lists[ListsName.English.NewsRecord];
+                            SPListItemCollection items = list.GetItems(spQuery);
+                            table = items.GetDataTable();
+                        }
+                        catch (Exception ex)
+                        {
+                            table = null;
+                        }
+                    }
+
+                }
+            });
+            return table;
         }
     }
 }
